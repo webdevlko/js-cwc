cb5747d60867548fefd0ac724e455bcde115c0ba
Perwaiz <71934428+webdevlko@users.noreply.github.com>
2024-03-30 07:45:30 +0000

    02 File done

---
+++ 01_basic/02_datatype.js
@@ -0,0 +1,24 @@
+//Major data types of JS, rest will check ECMA script https://tc39.es/ecma262/
+//number => 2 to power 53
+//bigint 
+//string
+//boolean => true false
+//null =>standalone value
+//undefined
+//symbol
+
+
+let age = 18;
+let isLoggedIn = false;
+let state = null;
+
+
+"use strict"; // treat all JS code as newer version
+
+//alert (3+3); 
+
+console.log(age);
+
+console.log(typeof age); //number
+console.log(typeof undefined); //undefined
+console.log(typeof null; //object
